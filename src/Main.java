2aa171079ff2b2f90dc37aa74f6cc7258c5765ed
NN <nik_rink@mail.ru>
2023-06-11 13:13:46 +0300

    Сделал ИСПРАВЛЕНИЯ для ДЗ-5,задачи 1-5, поместил в hw5 ветку

--- src/Main.java
+++ src/Main.java
@@ -10,31 +10,42 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задча 1:");
-        byte clientOS = 1;
-        if (clientOS == 0) {
+        int clientOS = 2;
+        int iOs = 0;
+        int android = 1;
+
+        if (clientOS == iOs) {
             System.out.println("Установите версию  приложения для iOS по ссылке");
-        } else if (clientOS == 1) {
+        } else if (clientOS == android) {
             System.out.println("Установите версию  приложения для Android по ссылке");
+        } else {
+            System.out.println("Эта ОС не поддерживается");
         }
+
     }
 
     public static void task2() {
         System.out.println("Задча 2:");
-        byte clientOS = 0;
+        int clientOS = 1;
+        int iOs = 0;
+        int android = 1;
         int clientDeviseYear = 2014;
+        int checkYear = 2015;
 
-        if (clientOS == 0) {
-            if (clientDeviseYear < 2015) {
+        if (clientOS == iOs) {
+            if (clientDeviseYear < checkYear) {
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке");
             } else {
                 System.out.println("Установите версию  приложения для iOS по ссылке");
             }
-        } else if (clientOS == 1) {
-            if (clientDeviseYear < 2015) {
+        } else if (clientOS == android) {
+            if (clientDeviseYear < checkYear) {
                 System.out.println("Установите облегчённую версию приложения для Android по ссылке");
             } else {
                 System.out.println("Установите версию  приложения для Android по ссылке");
             }
+        } else {
+            System.out.println("Эта ОС не поддерживается");
         }
     }
 
@@ -44,7 +55,9 @@ public class Main {
 
         if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             System.out.println(year + " год является высокосным");
-        } else System.out.println(year + " год не является высокосный");
+        } else {
+            System.out.println(year + " год не является высокосный");
+        }
     }
 
     public static void task4() {
@@ -60,23 +73,38 @@ public class Main {
         } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
             day += 2;
             System.out.println("Потребуется дней: " + day);
-        }
-
-        if (deliveryDistance >= 100) {
+        } else if (deliveryDistance >= 100) {
             System.out.println("Доставки нет");
         }
     }
 
     public static void task5() {
         System.out.println("Задча 5:");
-        int numberMonth = 10;
+        int numberMonth = 1;
 
         switch (numberMonth) {
-            case 12, 1, 2 -> System.out.println("Зима");
-            case 3, 4, 5 -> System.out.println("Весна");
-            case 6, 7, 8 -> System.out.println("Лето");
-            case 9, 10, 11 -> System.out.println("Осень");
-            default -> System.out.println("Не правельно введены данные");
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Не правильно введены данные");
         }
     }
 }
